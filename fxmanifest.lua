ee7b33991d8aca2be0fc44f53cfcea3298358cf4
Tom Grobbe <31419184+TomGrobbe@users.noreply.github.com>
2020-10-25 10:58:25 +0100

    Update cs project file, appveyor.yml and add fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+-- Manifest data
+fx_version 'bodacious'
+games {'gta5'}
+
+-- Resource stuff
+name 'DamageEvents'
+description 'A simple C# wrapper of the '
+version 'v1.0.1'
+author 'Tom Grobbe'
+url 'https://github.com/DevTestingPizza/DamageEvents/'
+
+client_script 'DamageEvents.net.dll'
+
+
+-- Config options:
+-- Set this to true to enable debug logs.
+enable_debug_prints_for_events 'false'
+
